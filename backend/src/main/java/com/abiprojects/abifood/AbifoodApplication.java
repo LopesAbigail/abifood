87117c4be5c751e28d97ddce91e103b5f7f47d5c
Abigail Lopes <contatoabigail_lopes@outlook.com>
2021-01-04 20:12:47 -0300

    Project created

---
+++ backend/src/main/java/com/abiprojects/abifood/AbifoodApplication.java
@@ -0,0 +1,13 @@
+package com.abiprojects.abifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbifoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbifoodApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/abiprojects/abifood/AbifoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abiprojects.abifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbifoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
