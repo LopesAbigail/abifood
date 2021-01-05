3999f5b911667eaaa343c663e3a08dca0d23868b
Abigail Lopes <contatoabigail_lopes@outlook.com>
2021-01-05 16:01:01 -0300

    POST/orders

--- backend/src/main/java/com/abiprojects/abifood/controllers/OrderController.java
+++ backend/src/main/java/com/abiprojects/abifood/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.abiprojects.abifood.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abiprojects.abifood.dto.OrderDTO;
 import com.abiprojects.abifood.services.OrderService;
@@ -28,4 +32,16 @@ public class OrderController {
 		// Retorna código 200 HTTP
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@PostMapping
+	// @RequestBody serve para converter JSON no nosso objeto JAVA DTO
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		// Para retornar o status 201 - criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	
 }
--- backend/src/main/java/com/abiprojects/abifood/services/OrderService.java
+++ backend/src/main/java/com/abiprojects/abifood/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.abiprojects.abifood.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.abiprojects.abifood.dto.OrderDTO;
+import com.abiprojects.abifood.dto.ProductDTO;
 import com.abiprojects.abifood.entities.Order;
+import com.abiprojects.abifood.entities.OrderStatus;
+import com.abiprojects.abifood.entities.Product;
 import com.abiprojects.abifood.repositories.OrderRepository;
+import com.abiprojects.abifood.repositories.ProductRepository;
 
 @Service
 // Permite que a classe seja injetada em outros componentes
@@ -18,6 +23,9 @@ public class OrderService {
 	@Autowired
 	// Container para injeção de dependência desacoplada
 	private OrderRepository ordertRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	// Retorna DTO: objeto enxuto que carrega apenas o que for especificado (não tem relação com o banco de dados)
 	@Transactional(readOnly = true)
@@ -28,4 +36,26 @@ public class OrderService {
 		return orderList.stream().map(order -> new OrderDTO(order)).collect(Collectors.toUnmodifiableList());
 	}
 	
+	@Transactional()
+	public OrderDTO insert(OrderDTO dto){
+	
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		
+		for(ProductDTO p: dto.getProducts()) {
+			// Instanciando produto SEM acessar o banco de dados
+			// Cria entidade gerenciável pelo JPA
+			/* Ao salvar o pedido, as associações de quais produtos são pertencentes
+			 * a ele também são salvas */
+			// Instanciando produto com base no Id do 'p' atual
+			// Preenche tabela de associação
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = ordertRepository.save(order);
+		
+		return new OrderDTO(order);
+		
+	}
 }
