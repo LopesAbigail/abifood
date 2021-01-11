5772b9324815fcd70cfc7f51b39256e1a3e22b50
Abigail Lopes <contatoabigail_lopes@outlook.com>
2021-01-11 08:50:30 -0300

    Fix total

--- backend/src/main/java/com/abiprojects/abifood/dto/OrderDTO.java
+++ backend/src/main/java/com/abiprojects/abifood/dto/OrderDTO.java
@@ -19,11 +19,25 @@ public class OrderDTO implements Serializable {
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
+	private Double total;
 	
 	// Para listar os produtos de dado pedido
 	private List<ProductDTO> products = new ArrayList<>();	
 	
 	public OrderDTO() {}
+
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status,
+			Double total, List<ProductDTO> products) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+		this.total = total;
+		this.products = products;
+	}
 
 	// Construtor que transforma a entidade Order em um OrderDTO
 	public OrderDTO(Order entity) {
@@ -33,6 +47,7 @@ public class OrderDTO implements Serializable {
 		longitude = entity.getLatitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
+		total = entity.getTotal();
 		
 		products = entity.getProducts().stream()
 				.map(x -> new ProductDTO(x)).collect(Collectors.toList());
@@ -84,6 +99,14 @@ public class OrderDTO implements Serializable {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+	
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
 	}
 
 	public List<ProductDTO> getProducts() {
